01615bb75b2a25b5469a7b8127a9efac2eaa7e80
gabriel rocha dias <gabigolbr36@gmail.com>
2025-01-30 21:42:57 -0300

    add files

---
+++ tela_inicial.py
@@ -0,0 +1,9 @@
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+
+class tela():
+    def __init__(self):
+        super().__init__()
+        
+            
