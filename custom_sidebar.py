efab2281ac9d2f6af930b510af14ef60ad2a484e
gabriel rocha dias <gabigolbr36@gmail.com>
2025-02-01 09:28:47 -0300

    add files

--- custom_sidebar.py
+++ custom_sidebar.py
@@ -75,22 +75,3 @@ class Sidebar(QWidget):
         self.animation.setEndValue(new_width)
         self.animation.start()      
 
-class janela_principal(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setFixedSize(900, 600)
-        self.widget_central = QWidget()
-        self.setCentralWidget(self.widget_central)
-        self.CentralLayout = QHBoxLayout()
-        self.widget_central.setStyleSheet("background-color: #161616;")
-        self.sideBar = Sidebar(self)
-        self.sideBar.setParent(self)
-        self.widget_central.setLayout(self.CentralLayout)
-        self.CentralLayout.addWidget(self.sideBar, alignment=Qt.AlignLeft)
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    tema = aplicar_tema_dark(app)
-    window = janela_principal()
-    window.show()
-    app.exec()        
--- tela_inicial.py
+++ tela_inicial.py
@@ -1,9 +1,26 @@
 from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
+from custom_sidebar import Sidebar
+from tema import aplicar_tema_dark
+import sys
 
-class tela():
+class janela_principal(QMainWindow):
     def __init__(self):
         super().__init__()
-        
-            
+        self.setFixedSize(900, 600)
+        self.widget_central = QWidget()
+        self.setCentralWidget(self.widget_central)
+        self.CentralLayout = QHBoxLayout()
+        self.widget_central.setStyleSheet("background-color: #161616;")
+        self.sideBar = Sidebar(self)
+        self.sideBar.setParent(self)
+        self.widget_central.setLayout(self.CentralLayout)
+        self.CentralLayout.addWidget(self.sideBar, alignment=Qt.AlignLeft)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    tema = aplicar_tema_dark(app)
+    window = janela_principal()
+    window.show()
+    app.exec()        
